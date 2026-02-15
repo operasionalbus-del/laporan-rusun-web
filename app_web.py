0f1b8d8ad730154304aca3bb72b3982df8691815
Rachmat <operasionalbus@gmail.com>
2026-02-15 20:29:36 +0700

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -35,7 +35,7 @@ def set_bg(image_file):
     )
 
 # pakai background rusunawa.jpg
-set_bg("rusunawa jt.jpeg")
+set_bg("Rusunawa Marunda.jpg")
 
 # =========================
 # UI
@@ -67,4 +67,5 @@ if uploaded_file and tanggal_target:
             )
 
         st.success("✅ File berhasil dibuat!")
+
 
