7caa4eeff102c8e050ff5f10a40ffdb56f8702bc
Rachmat <operasionalbus@gmail.com>
2026-02-16 10:09:23 +0700

    Update app_logic.py

--- app_logic.py
+++ app_logic.py
@@ -73,7 +73,9 @@ def filter_orderan_from_text(text, tanggal_target):
             if buffer:
                 reports.append("\n".join(buffer))
                 buffer = []
-
+            buffer.append(msg)
+            continue
+            
         if msg:
             buffer.append(msg)
 
@@ -220,4 +222,5 @@ def isi_template(template_path, chat_text, tanggal_target, output_file):
     wb.save(output_file)
     print("FILE SAVED:", output_file)
     return output_file
+
 
